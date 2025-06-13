18a502785e0c5d24bcf0eeed3feb33498471b138
meni0419 <meni0419@gmail.com>
2025-06-13 11:55:10 +0300

    HW1

--- test_simple_math.py
+++ test_simple_math.py
@@ -1,30 +1,36 @@
 import unittest
+import pytest
 from simple_math import SimpleMath
 
 
-class TestSimpleMath(unittest.TestCase):
-    def setUp(self):
-        self.math = SimpleMath()
+@pytest.fixture
+def test_math():
+    return SimpleMath()
 
-    def test_square_positive(self):
-        self.assertEqual(self.math.square(2), 4)
-        self.assertEqual(self.math.square(3), 9)
 
-    def test_square_zero(self):
-        self.assertEqual(self.math.square(0), 0)
+def test_square_positive(test_math):
+    assert test_math.square(2) == 4
 
-    def test_square_negative(self):
-        self.assertEqual(self.math.square(-2), 4)
 
-    def test_cube_positive(self):
-        self.assertEqual(self.math.cube(2), 8)
-        self.assertEqual(self.math.cube(3), 27)
+def test_square_zero(test_math):
+    assert test_math.square(0) == 0
 
-    def test_cube_zero(self):
-        self.assertEqual(self.math.cube(0), 0)
 
-    def test_cube_negative(self):
-        self.assertEqual(self.math.cube(-2), -8)
+def test_square_negative(test_math):
+    assert test_math.square(-2) == 4
+
+
+def test_cube_positive(test_math):
+    assert test_math.cube(2) == 8
+    assert test_math.cube(3) == 27
+
+
+def test_cube_zero(test_math):
+    assert test_math.cube(0) == 0
+
+
+def test_cube_negative(test_math):
+    assert test_math.cube(-2) == 8
 
 
 if __name__ == '__main__':
